5d5bbc1cb7b194dff08b701410d6d88d80b894a2
Dan Ichim <dan.ichim@assist.ro>
2016-01-16 10:31:18 +0200

    Add users controller

---
+++ public/controllers/users.js
@@ -0,0 +1,36 @@
+(function() {
+
+  'use strict';
+
+  function UsersCtrl($scope, MailsFactory, UsersFactory, $state) {
+
+    var self = this,
+      user = new UsersFactory();
+
+    self.login = function() {
+
+      var username = self.username || "",
+        password = self.password || "";
+
+      var login = user.login(username, password);
+
+      login
+        .then(function() {
+          $state.go('admin');
+        });
+
+    }
+
+    return ($scope.user = self);
+
+  }
+
+  UsersCtrl
+    .$inject = ['$scope', 'UsersFactory', '$state'];
+    
+
+  angular
+    .module('mainApp')
+    .controller('UsersCtrl', UsersCtrl);
+
+})();
---
+++ public/models/users.js
@@ -0,0 +1,35 @@
+(function() {
+  
+  "use strict";
+
+  function UsersFactory($resource) {
+
+    function Users() {}
+
+    Users.prototype = {
+
+      login: function(username, pass) {
+
+        var email = $resource('/auth/login');
+        return email.save({'username':username, password: pass}).$promise;
+      },
+
+      isAuthenticated: function() {
+
+        var email = $resource('/api/authenticated');
+        return email.get().$promise;
+      }
+
+    };
+
+    return Users;
+  }
+  
+  UsersFactory
+    .$inject = ['$resource'];
+  
+  angular
+    .module('mainApp')
+    .factory('UsersFactory', UsersFactory)
+
+}());
